aa9e64acf9aa230366d5799587e34f984d8fa2d4
joanneong <joanneongcf@u.nus.edu>
2020-06-02 19:01:50 +0800

    Implement up to checkbox functionality

--- hw4/FontChooser.js
+++ hw4/FontChooser.js
@@ -1,21 +1,51 @@
 class FontChooser extends React.Component {
 
-    constructor(props) {
-	super(props);
-    }
-    
+	constructor(props) {
+		super(props);
+		this.state = {
+			isFormHidden: true,
+		}
+	}
 
-    render() {
+	componentDidMount() {
+		this.setState({ 
+			isChecked: this.props.bold === 'true' ? true : false,
+			fontWeight: this.props.bold === 'true' ? 'bold' : 'normal'
+		});
 
-	return(
-	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden='true'/>
-	       <button id="decreaseButton" hidden='true'>-</button>
-	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-	       <button id="increaseButton" hidden='true'>+</button>
-	       <span id="textSpan">{this.props.text}</span>
-	       </div>
-	);
-    }
+	}
+
+	toggleFormElements() {
+		this.setState({ isFormHidden: !this.state.isFormHidden });
+	}
+
+	toggleFontWeight() {
+		var newWeight = this.state.fontWeight === 'bold' ? 'normal' : 'bold';
+		this.setState({ fontWeight: newWeight });
+	}
+
+	render() {
+		var size = this.props.size + 'px';
+		return(
+			<div>
+			<input type="checkbox" 
+				id="boldCheckbox" 
+				hidden={this.state.isFormHidden}
+				defaultChecked={this.state.isChecked}
+				onClick={this.toggleFontWeight.bind(this)}/>
+			<button id="decreaseButton" hidden={this.state.isFormHidden}>-</button>
+			<span id="fontSizeSpan" hidden={this.state.isFormHidden}>
+				{this.props.size}
+			</span>
+			<button id="increaseButton" hidden={this.state.isFormHidden}>+</button>
+			<span 
+				style={{ fontWeight: this.state.fontWeight, fontSize: size }} 
+				id="textSpan"
+				onClick={this.toggleFormElements.bind(this)}>
+				{this.props.text}
+			</span>
+			</div>
+		);
+  }
 }
 
