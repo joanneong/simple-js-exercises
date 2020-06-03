55126a322a5402d0338ed1e67a4913290a8032d6
joanneong <joanneongcf@u.nus.edu>
2020-06-03 20:33:29 +0800

    Implement part 3: marking a listitem

--- hw5/lists/src/ListItem.js
+++ hw5/lists/src/ListItem.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 
 class ListItem extends Component {
 
-    constructor(props) {
-	super(props);
-	this.state = { color: 'black' };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    handleClick() {
-	// Implement this function!
+  componentDidMount() {
+    this.setState({ color: 'black' });
+  }
+
+  handleClick() {
+    if (this.state.color === 'black') {
+      this.setState({ color: 'gray'});
+    } else {
+      this.setState({ color: 'black' })
     }
+  }
 
   render() {
     var item = this.props.item;
