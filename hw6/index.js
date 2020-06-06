993de0252ff5686cb9862efac60740cd49ae992b
joanneong <joanneongcf@u.nus.edu>
2020-06-06 22:40:03 +0800

    Implement add toys and add animals api

--- hw6/index.js
+++ hw6/index.js
@@ -4,6 +4,49 @@ var app = express();
 var Animal = require('./Animal.js');
 var Toy = require('./Toy.js');
 
+app.use('/findToy', (req, res) => {
+    var searchId = req.query.id;
+    Toy.findOne({ id: searchId }, (err, toy) => {
+        if (err || !toy) {
+            res.type('html').status(200);
+            res.json({});
+        } else {
+            res.json(toy);
+        }
+    });
+});
+
+app.use('/findAnimals', (req, res) => {
+    var query = {};
+
+    if (req.query.species) {
+        query.species = req.query.species;
+    }
+
+    if (req.query.trait) {
+        query.traits = req.query.trait;
+    }
+    
+    if (req.query.gender) {
+        query.gender = req.query.gender;
+    }
+
+    console.log(query);
+
+    if (req.query.species === undefined && req.query.trait === undefined
+        && req.query.gender === undefined) {
+        res.json({});
+    } else {
+        Animal.find(query, { traits: 0 }, (err, animals) => {
+            console.log(animals);
+            if (err || animals.length === 0) {
+                res.json({});
+            } else {
+                res.json(animals);
+            }
+        });
+    }
+});
 
 app.use('/', (req, res) => {
 	res.json({ msg : 'It works!' });
